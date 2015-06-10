f355a929926646d08fac5ab1ed4942428516cc2d
Kevin Lorenz <mail@kevinlorenz.com>
2015-06-10 21:11:19 +0200

    Add live reload (using browser extension)

--- gruntfile.js
+++ gruntfile.js
@@ -1,72 +1,80 @@
-
-
 'use strict';
 
 module.exports = function (grunt) {
 
-  // Show elapsed time after tasks run to visualize performance
-  require('time-grunt')(grunt);
-  // Load all Grunt tasks that are listed in package.json automagically
-  require('load-grunt-tasks')(grunt);
+    // Show elapsed time after tasks run to visualize performance
+    require('time-grunt')(grunt);
+    // Load all Grunt tasks that are listed in package.json automagically
+    require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-      shell: {
-        jekyllBuild: {
-          command: 'jekyll build'
+        // shell commands for use in Grunt tasks
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve'
+            }
         },
-        jekyllServe: {
-          command: 'jekyll serve'
-        }
-      },
 
-      watch: {
+        // watch for files to change and run tasks when they do
+        watch: {
+            sass: {
+                files: ['_scss/**/*.{scss,sass}'],
+                tasks: ['sass'],
+                options: { livereload: true } //using the browserextension
+            }
+        },
+
+        // sass (libsass) config
         sass: {
-          files: ['_scss/**/*.{scss, sass}'],
-          task: ['sass']
-        }
-      },
-
-      sass: {
-        options: {
-          sourceMap: true,
-          realtiveAssets: false,
-          outputStyle: 'expanded',
-          sassDir: '_scss',
-          cssDir: '_site/css'
+            options: {
+                sourceMap: true,
+                relativeAssets: false,
+                outputStyle: 'expanded',
+                sassDir: '_scss',
+                cssDir: '_site/css'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '_scss/',
+                    src: ['**/*.{scss,sass}'],
+                    dest: '_site/css',
+                    ext: '.css'
+                }]
+            }
         },
-        build: {
-          files: [{
-            expand: true,
-            cwd: '_scss',
-            src: ['**/*.{scss, sass}'],
-            dest: '_site/css',
-            ext: '.css'
-          }]
-        }
-      },
 
-      concurrent: {
-        serve: [
-          'sass',
-          'watch',
-          'shell:jekyllServe'
-        ],
-        options: {
-          logConcurrentOutpu: true
-        }
-      }
-  });
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'sass',
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
 
-  grunt.registerTask('serve', [
-    'concurrent:serve'
-  ]);
+    });
 
-  grunt.registerTask('build', [
-    'shell:jekyllBuild',
-    'sass'
-  ]);
+    // Register the grunt serve task
+    grunt.registerTask('serve', [
+        'concurrent:serve'
+    ]);
 
-  grunt.registerTask('default', 'build');
+    // Register the grunt build task
+    grunt.registerTask('build', [
+        'shell:jekyllBuild',
+        'sass'
+    ]);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'build');
+
 };
